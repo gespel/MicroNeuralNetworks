f73fbc54cf5dea43741a9231727dca03be2f7f62
gespel <heimbrodt@uni-potsdam.de>
2026-07-03 10:23:03 +0200

    bla

--- mnn.h
+++ mnn.h
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 typedef struct {
     int input_size;
@@ -12,4 +13,46 @@ typedef struct {
     Layer *layers;
 } MicroNeuralNetwork;
 
+float relu(float x) {
+    return x > 0 ? x : 0;
+}
 
+MicroNeuralNetwork* create_mnn(int num_layers, int *layer_sizes) {
+    MicroNeuralNetwork *mnn = (MicroNeuralNetwork *)malloc(sizeof(MicroNeuralNetwork));
+    mnn->num_layers = num_layers;
+    mnn->layers = (Layer *)malloc(num_layers * sizeof(Layer));
+    
+    for (int i = 0; i < num_layers; i++) {
+        mnn->layers[i].input_size = layer_sizes[i];
+        mnn->layers[i].output_size = layer_sizes[i + 1];
+        mnn->layers[i].weights = (float *)malloc(layer_sizes[i] * layer_sizes[i + 1] * sizeof(float));
+        mnn->layers[i].biases = (float *)malloc(layer_sizes[i + 1] * sizeof(float));
+        
+        // Initialize weights and biases (for simplicity, using random values)
+        for (int j = 0; j < layer_sizes[i] * layer_sizes[i + 1]; j++) {
+            mnn->layers[i].weights[j] = ((float)rand() / RAND_MAX) * 2 - 1; // Random weights between -1 and 1
+        }
+        for (int j = 0; j < layer_sizes[i + 1]; j++) {
+            mnn->layers[i].biases[j] = ((float)rand() / RAND_MAX) * 2 - 1; // Random biases between -1 and 1
+        }
+    }
+    
+    return mnn;
+}
+
+float inference(MicroNeuralNetwork *mnn, float *input) {
+    float *current_input = input;
+    for (int i = 0; i < mnn->num_layers; i++) {
+        Layer *layer = &mnn->layers[i];
+        float *output = (float *)malloc(layer->output_size * sizeof(float));
+        for (int j = 0; j < layer->output_size; j++) {
+            output[j] = layer->biases[j];
+            for (int k = 0; k < layer->input_size; k++) {
+                output[j] += current_input[k] * layer->weights[j * layer->input_size + k];
+            }
+            output[j] = relu(output[j]);
+        }
+        current_input = output;
+    }
+    return current_input[0]; // Assuming single output for simplicity
+}
--- test/tester.c
+++ test/tester.c
@@ -2,5 +2,12 @@
 
 
 int main() {
-    
+    MicroNeuralNetwork *mnn;
+    int layer_sizes[] = {3, 5, 2, 1}; // Example:
+    mnn = create_mnn(2, layer_sizes);
+
+    printf("Micro Neural Network created with %d layers.\n", mnn->num_layers);
+    float input[] = {1.0, 2.0, 3.0}; // Example input
+    float output = inference(mnn, input);
+    printf("Inference result: %f\n", output);
 }
