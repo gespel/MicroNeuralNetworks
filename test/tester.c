29e1460a5a47f3783d4ac3d1a653fff6c51aefa5
gespel <heimbrodt@uni-potsdam.de>
2026-07-03 10:44:31 +0200

    adding real random and outputting debug info

--- test/tester.c
+++ test/tester.c
@@ -3,11 +3,11 @@
 
 int main() {
     MicroNeuralNetwork *mnn;
-    int layer_sizes[] = {3, 5, 2, 1}; // Example:
-    mnn = create_mnn(2, layer_sizes);
+    int layer_sizes[] = {3, 5, 2, 2}; // Example:
+    mnn = create_mnn(3, layer_sizes);
 
     printf("Micro Neural Network created with %d layers.\n", mnn->num_layers);
     float input[] = {1.0, 2.0, 3.0}; // Example input
-    float output = inference(mnn, input);
-    printf("Inference result: %f\n", output);
+    float* output = inference(mnn, input);
+    printf("Inference result: %f %f\n", output[0], output[1]);
 }
